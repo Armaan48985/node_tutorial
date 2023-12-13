1640fea29250db9a7704623ed6e61e21cfcb7310
Armaan48985 <armaanthakur2308@gmail.com>
2023-12-13 21:54:50 +0530

    promises

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 ﻿// const os = require('os')
 // const user = os.userInfo()
 
+// const { read } = require('fs')
+
 // // console.log(os.type());
 
 // const {readFileSync, writeFileSync} = require('fs')
@@ -9,21 +11,34 @@
 // const second = "armaan"
 
 // writeFileSync('./greeting', `Your Text : ${first} ${second} !`, {flag: 'z'})
+const {readFile, writeFile} = require('fs').promises
 
-const http = require('http')
+const calling = async () => {
+    try{
+        const firstText = await readFile('./content/first.txt', 'utf8')
+        const secondText = await readFile('./content/second.txt', 'utf8')
+        await writeFile('./content/sabo-file.txt', `Sabo is ${firstText}'s and ${secondText}'s brother`)
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-        res.end("This is home page")
-    }                               
-    if(req.url === '/about'){
-        res.end("this is about page")
-    }                                
+        console.log(first, second)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+// const http = require('http')
+
+// const server = http.createServer((req, res) => {
+//     if(req.url === '/'){
+//         res.end("This is home page")
+//     }                               
+//     if(req.url === '/about'){
+//         res.end("this is about page")
+//     }                                
     
-    res.end(`
-        <h1>404 Error! Page Not Found</h1>
-        <a href="/">Back to home page</a>
-    `)
-})
+//     res.end(`
+//         <h1>404 Error! Page Not Found</h1>
+//         <a href="/">Back to home page</a>
+//     `)
+// })
 
-server.listen(3000)
+// server.listen(3000)
